7dc4dba183f18ab997d5e4e545e989bac577b18f
MEA\Anuj.Kalra <anuj.kalra@gds.ey.com>
2021-03-11 22:17:53 +0530

    dynamic shop page implementation and routing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom';
-import HomePage from './pages/homepage/homepage.component'
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS</h1>
-  </div>
-)
+import { Route, Switch } from 'react-router-dom';
+import HomePage from './pages/homepage/homepage.component';
+import Shop from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
+    
     <div>
+    <Header />
+      <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route path='/shop/hats' component={HatsPage} />
+      <Route path='/shop' component={Shop} />
+      </Switch>
     </div>
   );
 }
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import SHOP_DATA from './shop.data.js';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'
 
 class Shop extends React.Component{
     constructor(props){
         super(props);
 
         this.state = {
-            shopcollection : SHOP_DATA;
+            collection : SHOP_DATA
         }
     }
 
     render(){
+        const {collection} = this.state;
         return(
-            <div>
-                Shop Collection
+            <div className='shop-page'>
+                {collection.map(({id, ...otherCollection }) =>
+                   ( <CollectionPreview key={id} {...otherCollection} />
+                )
+                )}
             </div>
         )
     }
 }
 
+export default Shop;
